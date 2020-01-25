374f16e395f4484eb7030725663c68a276e79ae8
Maikel Maas <barbariantiger@gmail.com>
2020-01-25 10:44:12 +0100

    initial commit

---
+++ src/main/java/nl/maikel/RecipesBookApplication.java
@@ -0,0 +1,13 @@
+package nl.maikel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/maikel/RecipesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.maikel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
